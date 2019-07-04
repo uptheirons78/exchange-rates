93ce2d362c76e7cc3e23dcf45352a5b009630033
Mauro Bono <bono.mauro@gmail.com>
2019-07-04 17:42:14 +0200

    Updates

---
+++ precache-manifest.e289ca301704952b044890a92ef87f5c.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b4137464c4ced2c9eb0f57322a8ea294",
+    "url": "/exchange-rates/index.html"
+  },
+  {
+    "revision": "c44f67b7f6057ca30369",
+    "url": "/exchange-rates/static/css/main.2d3e37be.chunk.css"
+  },
+  {
+    "revision": "681b51057d74cfa8975d",
+    "url": "/exchange-rates/static/js/2.65aa1cca.chunk.js"
+  },
+  {
+    "revision": "c44f67b7f6057ca30369",
+    "url": "/exchange-rates/static/js/main.08b9b9c5.chunk.js"
+  },
+  {
+    "revision": "8fbef175c279bacdb80e",
+    "url": "/exchange-rates/static/js/runtime~main.b91f6f7b.js"
+  }
+]);
